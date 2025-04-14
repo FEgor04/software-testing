23fef31c9d5745c61fd0825004765ea48b715327
Egor Fedorov <fegor2004@gmail.com>
2025-04-14 21:42:38 +0300

    add stress test

--- lab4/src/gatling/kotlin/simulations/LoadTestSimulation.kt
+++ lab4/src/gatling/kotlin/simulations/LoadTestSimulation.kt
@@ -1,63 +1,67 @@
-package simulations
-import io.gatling.javaapi.core.CoreDsl.*
-import io.gatling.javaapi.http.HttpDsl.*
-import io.gatling.javaapi.core.*
-import io.gatling.javaapi.http.*
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.toJavaDuration
-class LoadTestSimulation : Simulation() {
-
-    private val httpProtocol = http
-        .baseUrl("http://stload.se.ifmo.ru:8080")
-        .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        .doNotTrackHeader("1")
-        .acceptLanguageHeader("en-US,en;q=0.5")
-        .acceptEncodingHeader("gzip, deflate")
-        .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
-
-    private val config1Scenario = scenario("Config 1 ($1700)")
-        .exec(http("request_1")
-            .get("?token=495356269&user=-2105802621&config=1")
-            .check(status().`is`(200))
-        )
-
-    private val config2Scenario = scenario("Config 2 ($3000)")
-        .exec(http("request_2")
-            .get("?token=495356269&user=-2105802621&config=2")
-            .check(status().`is`(200))
-        )
-
-    private val config3Scenario = scenario("Config 3 ($5500)")
-        .exec(http("request_3")
-            .get("?token=495356269&user=-2105802621&config=3")
-            .check(status().`is`(200))
-        )
-
-    init {
-        // Load test parameters
-        val users = 13 // Maximum number of parallel users
-        val requestsPerMinute = 40 // Average load per user
-        val rampUpTime = 30.seconds // Time to ramp up to full load
-        val testDuration = 5.minutes // Test duration
-
-        // Calculate request rate per second
-        val requestRate = (users * requestsPerMinute) / 60.0
-
-        setUp(
-            config1Scenario.injectOpen(
-                rampUsers(users).during(rampUpTime.toJavaDuration())
-            ).protocols(httpProtocol),
-            config2Scenario.injectOpen(
-                rampUsers(users).during(rampUpTime.toJavaDuration())
-            ).protocols(httpProtocol),
-            config3Scenario.injectOpen(
-                rampUsers(users).during(rampUpTime.toJavaDuration())
-            ).protocols(httpProtocol)
-        ).maxDuration(testDuration.toJavaDuration())
-         .assertions(
-            global().responseTime().max().lt(760), // Maximum response time should be less than 760ms
-            global().successfulRequests().percent().gt(95.0) // 95% of requests should be successful
-         )
-    }
-} 
+//package simulations
+//import io.gatling.javaapi.core.CoreDsl.*
+//import io.gatling.javaapi.http.HttpDsl.*
+//import io.gatling.javaapi.core.*
+//import io.gatling.javaapi.http.*
+//import kotlin.time.Duration.Companion.milliseconds
+//import kotlin.time.Duration.Companion.minutes
+//import kotlin.time.Duration.Companion.seconds
+//import kotlin.time.toJavaDuration
+//
+//class LoadTestSimulation : Simulation() {
+//
+//    private val httpProtocol = http
+//        .baseUrl("http://localhost:8080")
+//        .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+//        .doNotTrackHeader("1")
+//        .acceptLanguageHeader("en-US,en;q=0.5")
+//        .acceptEncodingHeader("gzip, deflate")
+//        .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+//
+//    private val config1Scenario = scenario("Config 3 ($1700)")
+//        .pace(1500.milliseconds.toJavaDuration())  // 40 requests/minute
+//        .exec(http("Config1 Request")
+//            .get("?token=495356269&user=-2105802621&config=1")
+//            .check(status().`is`(200)))
+//
+//    private val config2Scenario = scenario("Config 2 ($3000)")
+//        .pace(1500.milliseconds.toJavaDuration())  // 40 requests/minute
+//        .exec(http("Config2 Request")
+//            .get("?token=495356269&user=-2105802621&config=2")
+//            .check(status().`is`(200)))
+//
+//    private val config3Scenario = scenario("Config 1 ($5500)")
+//        .pace(1500.milliseconds.toJavaDuration())  // 40 requests/minute
+//        .exec(http("Config3 Request")
+//            .get("?token=495356269&user=-2105802621&config=3")
+//            .check(status().`is`(200)))
+//
+//    init {
+//        val totalUsers = 13
+//        val rampUpTime = 30.seconds
+//        val testDuration = 1.minutes
+//        val totalDuration = rampUpTime + testDuration
+//
+//        setUp(
+//            config1Scenario.injectClosed(
+//                rampConcurrentUsers(0).to(totalUsers).during(rampUpTime.toJavaDuration()),
+//                constantConcurrentUsers(totalUsers).during(testDuration.toJavaDuration())
+//            ).protocols(httpProtocol),
+//
+//            config2Scenario.injectClosed(
+//                rampConcurrentUsers(0).to(totalUsers).during(rampUpTime.toJavaDuration()),
+//                constantConcurrentUsers(totalUsers).during(testDuration.toJavaDuration())
+//            ).protocols(httpProtocol),
+//
+//            config3Scenario.injectClosed(
+//                rampConcurrentUsers(0).to(totalUsers).during(rampUpTime.toJavaDuration()),
+//                constantConcurrentUsers(totalUsers).during(testDuration.toJavaDuration())
+//            ).protocols(httpProtocol)
+//        )
+//            .maxDuration(totalDuration.toJavaDuration())
+//            .assertions(
+//                global().responseTime().max().lt(760),
+//                global().successfulRequests().percent().shouldBe(100.0)
+//            )
+//    }
+//}
--- lab4/src/gatling/kotlin/simulations/StressTestSimulation.kt
+++ lab4/src/gatling/kotlin/simulations/StressTestSimulation.kt
@@ -8,10 +8,11 @@ import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
+
 class StressTestSimulation : Simulation() {
 
     private val httpProtocol: HttpProtocolBuilder = http
-        .baseUrl("http://stload.se.ifmo.ru:8080")
+        .baseUrl("http://localhost:8080")
         .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
         .doNotTrackHeader("1")
         .acceptLanguageHeader("en-US,en;q=0.5")
@@ -21,29 +22,28 @@ class StressTestSimulation : Simulation() {
     // This will be modified based on the selected configuration from load testing
     private val selectedConfig = scenario("Selected Configuration Stress Test")
         .exec(http("request")
-            .get("?token=495356269&user=-2105802621&config=1") // Config number will be changed based on load test results
+            .get("/?token=495356269&user=-2105802621&config=1") // Added leading slash to make it absolute
             .check(status().`is`(200)))
 
 
     init {
         // Stress test parameters
-        val baseUsers = 13 // Starting number of users
-        val maxUsers = 50 // Maximum number of users to test
-        val stepUsers = 5 // Number of users to add in each step
-        val stepDuration = 2.minutes.toJavaDuration() // Duration of each step
-        val rampUpTime = 30.seconds.toJavaDuration() // Time to ramp up users in each step
+        val baseUsers = 50 // Starting number of users
+        val maxUsers = 100 // Maximum number of users to test
+        val stepDuration = 30.seconds.toJavaDuration() // Duration of each step
+        val rampUpTime = 5.seconds.toJavaDuration() // Time to ramp up users in each step
 
         // Create a list of injection steps
-        val injectionSteps = (baseUsers..maxUsers step stepUsers).map { users ->
-            rampUsers(users).during(rampUpTime)
+        val injectionSteps = (baseUsers..maxUsers step 10).map { users ->
+            constantConcurrentUsers(users).during(stepDuration)
         }
 
         setUp(
-            selectedConfig.injectOpen(*injectionSteps.toTypedArray())
+            selectedConfig.injectClosed(*injectionSteps.toTypedArray())
         ).maxDuration(stepDuration.multipliedBy((injectionSteps.size + 1).toLong()))
          .assertions(
             global().responseTime().max().lt(760), // Maximum response time should be less than 760ms
             global().successfulRequests().percent().gt(95.0) // 95% of requests should be successful
-         )
+         ) .protocols(httpProtocol)
     }
-} 
+}
